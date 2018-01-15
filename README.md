9032a45b2484a52ad6bf8341389be95e61aace8b
dwest24sc <dwest24@gmail.com>
2018-01-14 17:17:16 -0800

    unnumbered version is now latest

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
- OSC-Button
+1/13/18
+ OSC-Button verson 0.1
 # OSC-Button
+
+This is an all day endeavor to learn what a Pi is, how to code in Python
+and how to get a simple OSC Command (1 command) sent to a console or a QLAB machine.
+
+Current itteration has lots of hash marks for notes for me.
+OSC Commands currently set up for EOS
+
+long term goals will equal more buttons for stop and pause or back
+Plus a screen for current cue playback
+
+Ideas being used from various sources.  Including cscashby/pi-showcontrol.
--- osc-button.py
+++ osc-button.py
@@ -1,82 +1,55 @@
 #!/usr/bin/python3
 #$/home/pi/WLC-Button-OSC/osc-button.py
 
-#Jan. 18, 2017
-#First Attempt at Button Inputs
+#Jan. 14, 2017
+#Third Attempt at Button Inputs
 #Goal is to get the program to take a button Press on GPIO18 and Print "Go" *done
 #new goal: get OSC Commands to send. * Done
-#Goal- figure out how to allow for non linear button hits
+#Goal- figure out how to allow for non linear button hits *done
 #goal- Figure out how to get rid of the argument stuff and just send osc commands *done
+#next version may be for OSC feedback then a screen for displaying it
 
 #adding OSC stuff here first
-#import argparse
 import time
-#import random
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
-###the below was copied from python.org for learning., the aurgument number stuff was removed and
-#straight IP address and port were added
+#the below IP addres is what you are talking to and the listening port
+client = udp_client.SimpleUDPClient("192.168.1.69", 53001)
 
-#if __name__ == "__main__":
-#  parser = argparse.ArgumentParser()
-#  parser.add_argument("--ip", default="192.168.1.69",
- #     help="The ip of the OSC server")
- # parser.add_argument("--port", type=int, default=53001,
- #     help="The port the OSC server is listening on")
- # args = parser.parse_args()
+###ending the osc setup
 
-client = udp_client.SimpleUDPClient("192.168.1.69", 53001) #args.ip, args.port) and was indented
-
- 
-
-###ending the osc additions
-
+### Input setup here:
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-
 #Go Button
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 #Stop Button
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 #Pause Button
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#Print info While will send the command the whole time it's pressed
-#so we won't use that command.  left here for remembering later
-#while True:
-#    if(GPIO.input(18) == 0):
-#        print("Go")
-
-#the below isn't working
-#def printFunction(channel):
-#    print("Go")
-#    GPIO.add_event_detect(18, GPIO.FALLING, callback=printfunction, bouncetime=.1)
-    
-while True:
-#REMEBER TO INDENT UNTIL THIS IS DONE
-    GPIO.wait_for_edge(18, GPIO.FALLING)
-    print("GO")
-    client.send_message("eos/key/go", (1))
-#adjust the sleep to allow for forced pause between presses
-    #.5 is about the fastest it can go right now without
-    #counting both up and down
-    #which with wait for edge, it shouldn't be doing
-    time.sleep(.5)
-
-#below button hits don't function right, need to be in order,
-#can't go go, go , go
-#must happen go, stop, pause, go stop, and so on.
+#changed to event detection to allow for non linear pressing of buttons
+#Falling equals when you press rising equal release.
+#You could standby with a press and go with release.
 #
 
-#    GPIO.wait_for_edge(27, GPIO.FALLING)
-#    print("Stop")
-#    client.send_message("eos/key/stop", random.random())
-#    time.sleep(1)
+GPIO.add_event_detect(18, GPIO.FALLING, bouncetime=500)
+GPIO.add_event_detect(27, GPIO.FALLING, bouncetime=500)
+GPIO.add_event_detect(22, GPIO.FALLING, bouncetime=500)
 
-#    GPIO.wait_for_edge(22, GPIO.FALLING)
-#    print("Pause")
 
+start = time.time()
+while True:
+    if GPIO.event_detected(18):
+        print("GO")
+        client.send_message("eos/key/go", (0))
+    if GPIO.event_detected(27):
+        print("Stop")
+        client.send_message("eos/key/stop", (0))
+    if GPIO.event_detected(22):
+        print("Pause")
+        client.send_message("eos/key/pause", (0))
+    time.sleep(0.01)
+    
 GPIO.cleanup()
